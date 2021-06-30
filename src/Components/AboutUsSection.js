6cfc2d4de9f338da2d2caf1f9d86327fc9fc634a
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-06-30 15:53:38 -0400

    Set up components for homepage, installed styled components, got landing page design up

---
+++ src/Components/AboutUsSection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AboutUsSection() {
+    return (
+     <div className="aboutUs">
+         <div className="description">
+             <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, odio!</h2>
+         </div>
+     </div>
+    );
+  }
+  
+  export default AboutUsSection;
---
+++ src/Components/ContactSection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ContactSection() {
+    return (
+      <>
+        <div className="contact">
+            <h2>Any questions?</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci, voluptates.</p>
+        </div>
+      </>
+    );
+  }
+  export default ContactSection;
