853ab3b570688a39a80e88e64e4fb68e18e089d5
Hannah Lee <013hannah.a.lee@gmail.com>
2021-07-17 13:06:58 -0500

    users can now contact us. An email will be sent to an email box. Page will reload after letting the user know the email was succcessful

--- client/src/Components/ContactSection.js
+++ client/src/Components/ContactSection.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Styled from 'styled-components';
-// import GitHub from '@material-ui/icons/GitHub';
-// import LinkedIn from '@material-ui/icons/LinkedIn';
+import emailjs from 'emailjs-com';
+import { Link } from 'react-router-dom';
 
 
 function ContactSection() {
+
+  const [formState, setFormState] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    message: '',
+  });
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // submit form
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_o2ic2u7', 'template_btxt32c', e.target, 'user_C9L2p6Qq06OVGTs81D7Dp')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+
+    document.getElementById("messageSent").style.visibility = "visible";
+    setTimeout(function(){
+      console.log('reloaded')
+      window.location.reload(1);
+   }, 2000);
+  };
 
   return (
 
@@ -12,29 +48,30 @@ function ContactSection() {
       <ContactHeader>
         <h1 id="Contact"><span style={{ color: "#FFC947" }}>.</span>contact</h1>
       </ContactHeader>
-      <Form>
+      <Form onSubmit={handleFormSubmit}>
         <InsideForm>
           <FormHeader>
             <h2>We would love to hear from you.</h2>
           </FormHeader>
           <FormGroup>
             <FormLabel>First Name:</FormLabel>
-            <FormInput />
+            <FormInput type="text" name="firstName" id="first-name" placeholder="First Name" value={formState.firstName} onChange={handleChange} />
           </FormGroup>
           <FormGroup>
             <FormLabel>Last Name:</FormLabel>
-            <FormInput />
+            <FormInput type="text" name="lastName" id="last-name" placeholder="Last Name" value={formState.lastName} onChange={handleChange} />
           </FormGroup>
           <FormGroup>
             <FormLabel>Email:</FormLabel>
-            <FormInput />
+            <FormInput type="email" name="email" id="email" placeholder="Your email" value={formState.email} onChange={handleChange} />
           </FormGroup>
           <FormGroup>
             <FormLabel>Message:</FormLabel>
-            <MessageTextArea/>
+            <MessageTextArea type="text" name="message" id="message" placeholder="Please write your message to us!" onChange={handleChange} value={formState.message} />
           </FormGroup>
           <div>
             <button>Submit</button>
+            <h4 id="messageSent" style={{ display: "inline", paddingLeft: "1em", visibility: "hidden" }}>Thank you! Your message has been sent.</h4>
           </div>
         </InsideForm>
       </Form>
@@ -61,7 +98,7 @@ const ContactHeader = Styled.div
     margin: 3rem 0;
   `;
 
-  const Form = Styled.form
+const Form = Styled.form
   `
       height: 100vh;
       display: flex;
@@ -69,8 +106,8 @@ const ContactHeader = Styled.div
       justify-content: center;
       text-align: left;
   `;
- 
-  const InsideForm = Styled.div 
+
+const InsideForm = Styled.div
   `
       display: block;
       position: relative;
@@ -79,20 +116,20 @@ const ContactHeader = Styled.div
       color: #0A1931;
   `;
 
-    const FormHeader = Styled.h2
+const FormHeader = Styled.h2
   `
       padding: 2rem 0;
       color: #EFEFEF;
   `;
 
-  const FormLabel = Styled.label
-`
+const FormLabel = Styled.label
+  `
     display: block;
     margin-bottom: 5px;
     color: #EFEFEF;
 `;
 
-  const FormGroup = Styled.div
+const FormGroup = Styled.div
   `
       display: block;
       position: relative;
@@ -101,8 +138,8 @@ const ContactHeader = Styled.div
       color: #0A1931;
   `;
 
-  const FormInput = Styled.input
-`
+const FormInput = Styled.input
+  `
     display: block;
     width: 100%;
     padding: 10px 15px;
@@ -112,7 +149,7 @@ const ContactHeader = Styled.div
 `;
 
 const MessageTextArea = Styled.textarea
-`
+  `
     display: block;
     width: 100%;
     height: 100px;
@@ -121,6 +158,5 @@ const MessageTextArea = Styled.textarea
     border-radius: 6px;
     transition: 0.4s;
 `;
-
 
 export default ContactSection;
